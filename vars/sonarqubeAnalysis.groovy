175e1f03cab57b6466a564f0c8ab5dd1d34e58a5
geoffrey <1377499035@qq.com>
2024-08-31 20:51:38 +0800

    modify sonarqube

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(){
   withSonarQubeEnv('sonar-server') {
-    sh ''' $SONAR_SCANNER_HOME/bin/sonar-scanner \
+    sh ''' $SCANNER_HOME/bin/sonar-scanner \
     -Dsonar.projectKey=Youtube \
     -Dsonar.projectName=Youtube
     '''
