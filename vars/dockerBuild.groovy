a4be78635670ad97f1abe2f42d33cac27e1c4649
geoffrey <1377499035@qq.com>
2024-08-31 22:49:40 +0800

    modify builddocker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String dockerHubUsername, String dockerImageName) {
-  sh "docker build --build-arg REACT_APP_RAPID_API_KEY=d6c622a8b8msh73798ba27b77ce8p15e572jsnc3e9cb1e6aa7 -t ${dockerImageName}"
+  sh "docker build --build-arg REACT_APP_RAPID_API_KEY=d6c622a8b8msh73798ba27b77ce8p15e572jsnc3e9cb1e6aa7 -t ${dockerImageName} ."
   sh "docker tag ${dockerImageName} ${dockerHubUsername}/${dockerImageName}:latest"
 
   withDockerRegistry([credentialsId: 'docker',url: "https://index.docker.io/v1/"]) {
